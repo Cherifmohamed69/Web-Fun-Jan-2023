016ae9eff5cf7b7d1b085c11226fa998340ec16c
Cherifmohamed69 <coachcherifmohamed@gmail.com>
2023-05-08 20:08:24 +0200

    Fizzbuzz

--- Week2/Day 1/Practice/Fizzbuzz/Fizzbuzz.js
+++ Week2/Day 1/Practice/Fizzbuzz/Fizzbuzz.js
@@ -1,3 +1,6 @@
+
+// solution 1
+
 var index = 1;
 while (index<101) {
     if(index%3 == 0 & index%5 != 0){
@@ -15,5 +18,25 @@ while (index<101) {
     }
     
     index+=1;
+}
+
+
+// solution 2
+
+for (var index2 = 1 ; index2 <101; index2++) {
+    if(index2%3 == 0 & index2%5 != 0){
+        console.log ("Fizz");   
+    }
+    if(index2%5 == 0 & index2%3 != 0){
+        console.log ("Buzz");   
+    }
+    if( index2%3 ==0 & index2%5 == 0) {
+        console.log( "FizzBuzz");
+    }
+
+    if(index2%3 != 0 & index2%5 != 0){
+        console.log (index2);
+    }
+    
 }
 
