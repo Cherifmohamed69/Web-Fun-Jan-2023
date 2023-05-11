5a3bf89c02f28c327cd4a07a31ccc5b59ad2f585
Cherifmohamed69 <coachcherifmohamed@gmail.com>
2023-05-12 01:54:45 +0200

    likes

---
+++ Week2/Day 2/Core/likes/likess - Copie/scrip.js
@@ -0,0 +1,13 @@
+function add(id) {
+    const myElement = document.getElementById(id).innerHTML;
+    var number = Number(myElement);
+    number+= 1;
+    document.getElementById(id).innerHTML = number;
+
+}
+
+
+
+
+    
+
---
+++ Week2/Day 2/Core/likes/script.js
@@ -0,0 +1,9 @@
+
+
+function add() {
+  var likeCount = document.querySelector("#likesss");
+  console.log(likeCount);
+  var Count=parseInt(likeCount.innerHTML);
+  Count++;
+  likeCount.innerHTML=count;
+}
