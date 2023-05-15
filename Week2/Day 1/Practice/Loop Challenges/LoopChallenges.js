89c4ecf01837dd99d47500d31a96f0f0b58cf8e6
Cherifmohamed69 <coachcherifmohamed@gmail.com>
2023-05-15 11:08:06 +0200

    loopchalenges

--- Week2/Day 1/Practice/Loop Challenges/LoopChallenges.js
+++ Week2/Day 1/Practice/Loop Challenges/LoopChallenges.js
@@ -1,10 +1,26 @@
-for (var index = 1 ; index <21 ; index++ ) {   
-if (index < 21 );
-console.log (index)
+
+for(var i=1; i<20; i+=2) {
+    console.log(i);
 }
 
-for ( var index2 = 100 ; index2 >=0 ; index2--) {
-    if (index2 %3 == 0) {  
-    console.log (index2);
+for(var i=100; i>-1; i--) {
+    if(i % 3 == 0) {
+        console.log(i);
+    }
 }
- }
+
+for(var i=4; i>-4; i-=1.5) {
+    console.log(i);
+}
+
+var sum = 0;
+for(var i=1; i<101; i++) {
+    sum += i;
+}
+console.log(sum);
+
+var product = 1;
+for(var i=1; i<13; i++) {
+    product *= i;
+}
+console.log(product);
